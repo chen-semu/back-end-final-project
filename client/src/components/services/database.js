8b607d4ddc52e6b8a66def8ba931878ed349c7be
chen-semu <chensemu@gmail.com>
2022-12-05 20:58:58 +0200

    adding Api keys from mongo DB

--- client/src/components/services/database.js
+++ client/src/components/services/database.js
@@ -2,25 +2,25 @@
 const basicAPI='http://localhost:8080'
 
 const fetchCities= async ()=>{
-  return await fetch(`${basicAPI}/cities`)
+  return await fetch(`https://us-east-1.aws.data.mongodb-api.com/app/application-0-jffdt/endpoint/cities`)
   .then(res=>res.json())
   .catch((error)=>{console.log(error);})
 }
 
 const fetchOrders=async ()=>{
-  return await fetch(`${basicAPI}/orders`)
+  return await fetch(`https://us-east-1.aws.data.mongodb-api.com/app/application-0-jffdt/endpoint/orders`)
     .then(res=>res.json())
     .catch((error)=>{console.log(error);})
   }
 
 const fetchStores=async ()=>{
-  return await fetch(`${basicAPI}/stores`)
+  return await fetch(`https://us-east-1.aws.data.mongodb-api.com/app/application-0-jffdt/endpoint/stores`)
     .then(res=>res.json())
     .catch((error)=>{console.log(error);})
   }
 
 const fetchProducts=async ()=>{
-    return await fetch(`${basicAPI}/products`)
+    return await fetch(`https://us-east-1.aws.data.mongodb-api.com/app/application-0-jffdt/endpoint/products`)
     .then(res=>res.json())
     .catch((error)=>{console.log(error);})
   }
